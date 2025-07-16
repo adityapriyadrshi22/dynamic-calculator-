d6f2ad6781ebd86c795b2fdee88c40f2d6ef1a3e
aditya priydarshi <adityapri06@gmail.com>
2025-07-16 23:21:30 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,5 +1,27 @@
-function dis(n){
-  
-    document.getElementById("result").value +=n
-     
+
+function dis(n) {
+    console.log(`Button ${n} pressed`);
+    document.getElementById("expression").value += n;
 }
+
+
+function clr() {
+    document.getElementById("expression").value = "";
+    document.getElementById("result").value = "";
+}
+
+
+function solve() {
+    try {
+        const expr = document.getElementById("expression").value;
+        const output = eval(expr); 
+
+        
+        document.getElementById("result").value = `= ${output}`;
+        console.log(`${expr} = ${output}`);
+    } catch (err) {
+        document.getElementById("result").value = "Error";
+    }
+}
+
+
